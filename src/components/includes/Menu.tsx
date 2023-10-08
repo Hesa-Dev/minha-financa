6a202750e7fedcb6376aaffa979f2e450daa7bd8
hesa-dev <geral@hesasoft.com>
2023-10-08 19:49:10 +0200

    first commit

---
+++ src/components/includes/Menu.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Menu() {
+
+
+    return (
+        <nav className="flex  bg-slate-600 text-2xl">
+            
+            <ul>
+                <li>
+                    Minha Área 
+                </li>
+                <li>
+                    Sobre Nós 
+                </li>
+            </ul>
+        </nav>
+    )
+}
